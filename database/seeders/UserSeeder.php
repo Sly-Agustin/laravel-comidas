a2c51fa4eac8c9205580057312db3406ca4005e2
Agustin Sly <agustin_sly@hotmail.com>
2021-05-13 13:57:45 -0300

    Modelos
    Agregados modelo de prueba para los ingredientes y su migracion correspondiente

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::Truncate();
+        for ($i = 1; $i <= 10; $i++) {
+            $usuario = new User;
+            $usuario->name = 'Usuario '.$i;
+            $usuario->email = 'usuario'.$i.'@gmail.com';
+            $usuario->password = Hash::make('contrasenia');
+            //$usuario->api_token = Str::random(60);
+            $usuario->api_token = bin2hex(openssl_random_pseudo_bytes(30));
+            $usuario->isAdmin = false;
+            $usuario->save();
+        }
+
+        // \App\Models\User::factory()->count(10)->create();  Ver como usar el factory más adelante
+    }
+}
