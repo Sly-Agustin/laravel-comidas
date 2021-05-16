77c2160d26f52d06c16646701eec796440d3ae66
Agustin Sly <agustin_sly@hotmail.com>
2021-05-16 14:57:28 -0300

    Agregado middleware
    - Agregado el middleware que diferencia usuarios comunes de admines. Actualmente solo hay una ruta admin puesta para testeo

---
+++ resources/views/inicioAdmin.blade.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<html lang="en">
+    <p>Soy la pagina de inicio de admin provisoria. Si estás viendo esto y sos admin significa que el middleware funciona bien! Si no sos admin y estas viendo esto significa que el middleware no funciona como debería</p>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Grupo admin, las rutas que solo puedan ser accedidas por administradores van acá
+Route::group(['middleware' => ['admin']], function () {
+    Route::get('admin-view', [App\Http\Controllers\PagesController::class, 'inicioAdmin'])->name('admin.view');
+});
