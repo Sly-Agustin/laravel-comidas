a96bd945369af5b241ea82ce2593013ddef918e0
Agustin Sly <agustin_sly@hotmail.com>
2021-06-20 18:58:27 -0300

    Update plantilla.blade.php
    Commit para ver si funcionan los estilos en https.

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -8,9 +8,9 @@
         <!-- Bootstrap CSS -->
         @yield('stylesheets')
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('css/plantilla.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/button.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/plantilla.css') }}" type="text/css">
+        <link rel="stylesheet" href="{{ asset('css/button.css') }}" type="text/css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
         </style>
